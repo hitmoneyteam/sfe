261d43d879b9a2d025c1a42bbd151fd4c2c70a8a
hitmoneyteam <84968139+hitmoneyteam@users.noreply.github.com>
2021-08-10 18:15:03 +0200

    Update verifier.js

--- deployment/verifier.js
+++ deployment/verifier.js
@@ -4,7 +4,7 @@ const exec = util.promisify(require('child_process').exec)
 
 const contractName = "LPLottery"
 const contractAddress = "0x95F198a54f824fEA09531B21191884C6a5ACEF75"
-const network = "testnet"
+const network = "bsc"
 
 const verify = async (_contractName, _contractAddress, _network) => {
     console.log("\nVerifying ...")

0095da602258b3f493ae340767f7d459a2104d28
hitmoneyteam <84968139+hitmoneyteam@users.noreply.github.com>
2021-08-26 13:57:19 +0200

    Update LpLottery.sol

--- contracts/LpLottery.sol
+++ contracts/LpLottery.sol
@@ -22,47 +22,81 @@ contract LpLottery is PancakeClass {
     }
 
     function transferOwnership(address newManger) public restricted {
-        admin=newManger;
+        admin = newManger;
     }
 
-    function createContext(uint256 _prizeValue) public restricted {
-       lotteryStructs[lotteryId] = new Lottery(_prizeValue, address(this));
-       lotteryId++;
-       emit LotteryCreated(lotteryId);
+    function createContext(uint256 _prizeValue, uint256 _minimumAmount)
+        public
+        restricted
+    {
+        lotteryStructs[lotteryId] = new Lottery(
+            _prizeValue,
+            _minimumAmount,
+            address(this)
+        );
+        lotteryId++;
+        emit LotteryCreated(lotteryId);
     }
 
     function declareWinner(uint256 _lotteryId) public restricted {
         require(!lotteryWinnerDeclared[_lotteryId], "Winner already declared");
         lotteryWinnerDeclared[_lotteryId] = true;
         address winner = lotteryStructs[_lotteryId].declareWinner();
-        lotteryWinner[_lotteryId]=winner;
-        uint256 tokensGetInSafemars = convertBUSDToSafeMars(lotteryStructs[_lotteryId].getPrizeValue());
+        lotteryWinner[_lotteryId] = winner;
+        uint256 tokensGetInSafemars = convertBUSDToSafeMars(
+            lotteryStructs[_lotteryId].getPrizeValue()
+        );
         IERC20(tokenSafeMars).transfer(winner, tokensGetInSafemars);
     }
 
-    function getEntriesRequired(uint256 _lotteryId) public view returns(uint256){
+    function getEntriesRequired(uint256 _lotteryId)
+        public
+        view
+        returns (uint256)
+    {
         return lotteryStructs[_lotteryId].getEntriesRequired();
     }
 
-    function viewWinner(uint256 _lotteryId) public view returns(address) {
-        require(lotteryWinnerDeclared[_lotteryId], "Winner is not declared yet");
+    function viewWinner(uint256 _lotteryId) public view returns (address) {
+        require(
+            lotteryWinnerDeclared[_lotteryId],
+            "Winner is not declared yet"
+        );
         return lotteryWinner[_lotteryId];
     }
 
-    function participateInBusd(uint256 _lotteryId, uint256 amount) public nonReentrant {
+    function participateInBusd(uint256 _lotteryId, uint256 amount)
+        public
+        nonReentrant
+    {
+        require(
+            amount >= lotteryStructs[_lotteryId].getMinimumAmountInBusd(),
+            "Transferred amount is less than minimum amount"
+        );
         IERC20(tokenBUSD).transferFrom(msg.sender, address(this), amount);
-
         uint256 entryFee = amount.mul(4).div(100);
         lotteryStructs[_lotteryId].participate(entryFee, msg.sender);
         uint256 stakingAmount = amount.sub(entryFee);
         stakeInBUSD(stakingAmount, msg.sender);
     }
 
-    function participateInSafemars(uint256 _lotteryId, uint256 amount) public nonReentrant {
+    function participateInSafemars(uint256 _lotteryId, uint256 amount)
+        public
+        nonReentrant
+    {
+        uint256 tokensGetInBusdForTotalAmount = convertSafeMarsToBUSD(amount);
+        require(
+            tokensGetInBusdForTotalAmount >=
+                lotteryStructs[_lotteryId].getMinimumAmountInBusd(),
+            "Transferred amount is less than minimum amount"
+        );
+
         uint256 initBalance = IERC20(tokenSafeMars).balanceOf(address(this));
         IERC20(tokenSafeMars).transferFrom(msg.sender, address(this), amount);
-        amount = IERC20(tokenSafeMars).balanceOf(address(this)).sub(initBalance);
-        
+        amount = IERC20(tokenSafeMars).balanceOf(address(this)).sub(
+            initBalance
+        );
+
         uint256 entryFee = amount.mul(4).div(100);
         uint256 tokensGetInBusd = convertSafeMarsToBUSD(entryFee);
         lotteryStructs[_lotteryId].participate(tokensGetInBusd, msg.sender);
@@ -81,7 +115,5 @@ contract LpLottery is PancakeClass {
         _;
     }
 
-    event LotteryCreated(
-        uint256 lotteryId
-    );
+    event LotteryCreated(uint256 lotteryId);
 }
